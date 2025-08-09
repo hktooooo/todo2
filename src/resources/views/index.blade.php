dc721f62a76e81f9c2cfcde223b8f4c8798ff978
kato hideaki <mamamacha88@gmail.com>
2025-08-09 10:10:18 +0900

    リモートリポジトリ変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -21,6 +21,7 @@
 <!-- @dump($todos) -->
 
 <div class="todo__content">
+<<<<<<< HEAD
   <!-- 新規作成欄 -->
   <div>
     <h2 class="title__form">新規作成</h2>
@@ -99,6 +100,41 @@
       </form>
     </div>
     @endforeach
+=======
+  <form class="form" action="/contacts/confirm" method="post">
+  @csrf
+    <div class="todo__group">
+      <div class="todo__input">
+        <input type="text" name="name" value="{{ old('name') }}" />
+      </div>
+      <div class="todo__button">
+        <button class="todo__button-submit" type="submit">作成</button>
+      </div>
+    </div>
+  </form>
+
+  <div>
+    <div class="todo__heading">
+      <h2>Todo</h2>
+    </div>
+
+    <div class="todo__list">
+      <div class="todo__item">
+        <p class="todo__item__content">test</p>
+        <div class="todo__item__button">
+          <button class="todo__item__button-submit" type="submit">更新</button>
+          <button class="todo__item__button-submit" type="submit">削除</button>
+        </div>
+      </div>
+      <div class="todo__item">
+        <p class="todo__item__content">test2</p>
+        <div class="todo__item__button">
+          <button class="todo__item__button-submit" type="submit">更新</button>
+          <button class="todo__item__button-submit" type="submit">削除</button>
+        </div>
+      </div>    
+    <div>
+>>>>>>> ff4ba6b (update)
   </div>
 </div>
 @endsection
